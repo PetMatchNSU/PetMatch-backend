6c26d06bf4d8f07fbe8636c9f58ff3a91d1510a2
Курдюков Данил <d.kurdyukov@g.nsu.ru>
2025-09-14 19:17:30 +0700

    Initial commit

---
+++ src/main/java/org/nsu/PetMatch.java
@@ -0,0 +1,13 @@
+package org.nsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMatch {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetMatch.class, args);
+    }
+
+}
