5bfb16941b44a0a3dd170c727c9bfdf34af5e0fb
wiseConst <ash.ketchi1010@gmail.com>
2025-09-30 12:28:00 +0700

    Init commit.

---
+++ src/main/java/org/nsu/animal/entity/AnimalCardFile.java
@@ -0,0 +1,46 @@
+package org.nsu.animal.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.nsu.users.entity.File;
+
+@Entity
+@Table(name = "t_animals_cards_files")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnimalCardFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_animal_card", nullable = false)
+    @ToString.Exclude
+    private AnimalCard animalCard;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_file", nullable = false)
+    @ToString.Exclude
+    private File file;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_type", nullable = false)
+    @ToString.Exclude
+    private AnimalCardFileType type;
+}
---
+++ src/main/java/org/nsu/files/entity/File.java
@@ -0,0 +1,41 @@
+package org.nsu.files.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "t_files")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_type", nullable = false)
+    @ToString.Exclude
+    private FileType type;
+
+    @Column(name = "link", nullable = false, length = 36)
+    private String link;
+}
