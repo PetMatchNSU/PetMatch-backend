71462066f361c2984d0811ea0eee88c40fc92a6b
Constantine Galkin <89094738+wiseConst@users.noreply.github.com>
2025-10-08 20:10:50 +0700

    [PetMatch-15175] Инициализация таблиц файлов
    * Init commit.
    
    * fixes
    
    * removed toString lombok annotation

---
+++ src/main/java/org/nsu/animal/entity/AnimalCardFile.java
@@ -0,0 +1,43 @@
+package org.nsu.animal.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.nsu.users.entity.File;
+
+@Entity
+@Table(name = "t_animals_cards_files")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnimalCardFile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_animal_card", nullable = false)
+    private AnimalCard animalCard;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_file", nullable = false)
+    private File file;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_type", nullable = false)
+    private AnimalCardFileType fileType;
+}
