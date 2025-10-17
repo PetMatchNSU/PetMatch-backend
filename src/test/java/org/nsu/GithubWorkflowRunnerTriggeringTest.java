606e23b1ac7c076520cd313de622a5ec2d73e1eb
Andrey Valitov <andrey.valitov@ligastavok.ru>
2025-10-17 13:12:43 +0700

    [PetMatch-15647] Init github workflow

---
+++ src/test/java/org/nsu/GithubWorkflowRunnerTriggeringTest.java
@@ -0,0 +1,13 @@
+package org.nsu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GithubWorkflowRunnerTriggeringTest {
+
+    @Test
+    void givenGithubWorkflowSelfRunner_whenPushCodeOnPullRequest_thenShouldTriggerTests() {
+        assertEquals(5, 5);
+    }
+}
