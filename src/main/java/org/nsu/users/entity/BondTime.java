12b3c7c9d3ce7aa277a0e2fa90bbdea1f23bbe59
Zertalian1 <91644941+Zertalian1@users.noreply.github.com>
2025-10-19 20:48:25 +0700

    [PetMatch-15683] Добавить таблицу t_bond_times (#10)
    * [PetMatch-15683] [back] добавить таблицу t_bond_times

---
+++ src/main/java/org/nsu/users/entity/BondTime.java
@@ -0,0 +1,40 @@
+package org.nsu.users.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "t_bond_times")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BondTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "start", nullable = false)
+    private LocalTime startContactTime;
+
+    @Column(name = "end", nullable = false)
+    private LocalTime endContactTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_user", insertable = false, updatable = false)
+    private User user;
+}
