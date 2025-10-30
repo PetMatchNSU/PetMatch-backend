9930bc03a783f7cb766d5381b05eadefa3f87ab1
a.stolyarov <a.stolyarov@g.nsu.ru>
2025-10-30 17:45:58 +0700

    added build infra; added t_users_autorities junction table

--- src/main/java/org/nsu/PetMatch.java
+++ src/main/java/org/nsu/PetMatch.java
@@ -2,13 +2,11 @@ package org.nsu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication(scanBasePackages = "org.nsu")
 @EnableJpaRepositories(basePackages = "org.nsu")
-@EntityScan(basePackages = "org.nsu")
 @EnableCaching
 public class PetMatch {
 
