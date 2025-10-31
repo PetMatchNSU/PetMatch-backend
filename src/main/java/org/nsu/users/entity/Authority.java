a7de99ec64affcd9319ecf659718184fdeecbd4d
a.stolyarov <a.stolyarov@g.nsu.ru>
2025-10-31 15:20:06 +0700

    many to many implementation is JWUtils compliant now

--- src/main/java/org/nsu/users/entity/Authority.java
+++ src/main/java/org/nsu/users/entity/Authority.java
@@ -1,14 +1,28 @@
 package org.nsu.users.entity;
 
+import org.nsu.users.entity.userAuthority.UserAuthority;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "t_authorities")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Authority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +33,7 @@ public class Authority {
 
     @Column(unique = true, nullable = false, length = 256)
     private String description;
+
+    @OneToMany(mappedBy = "authority", fetch = FetchType.LAZY)
+    private Set<UserAuthority> userAuthorities = new HashSet<>();
 }
--- src/main/java/org/nsu/users/entity/User.java
+++ src/main/java/org/nsu/users/entity/User.java
@@ -1,5 +1,9 @@
 package org.nsu.users.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -10,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -58,6 +63,11 @@ public class User {
     @JoinColumn(name = "id_region", nullable = false)
     @ToString.Exclude
     private Region region;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_authority", nullable = false)
+    @ToString.Exclude
+    private Set<Authority> authorities = new HashSet<>();// JWUtils are dependent on it
 
     private boolean isEmailVerified = false;
 
