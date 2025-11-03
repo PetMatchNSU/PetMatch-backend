8a5b7bb32443fe7971c94505c58a6c2f25ab5a47
Constantine Galkin <89094738+wiseConst@users.noreply.github.com>
2025-11-04 00:08:20 +0700

    pattern fixes

--- src/main/java/org/nsu/users/utils/ValidationPatterns.java
+++ src/main/java/org/nsu/users/utils/ValidationPatterns.java
@@ -4,8 +4,7 @@ public final class ValidationPatterns {
     private ValidationPatterns() {}
 
     /**
-     * Pattern matching time in HH:mm 24-hour format (00:00 - 23:59)
+     * Jackson/DateTime pattern for LocalTime formatting/parsing
      */
-    public static final String TIME_HH_MM = "^(?:[01]\\d|2[0-3]):[0-5]\\d$";
-    
+    public static final String TIME_FORMAT = "HH:mm";
 }
