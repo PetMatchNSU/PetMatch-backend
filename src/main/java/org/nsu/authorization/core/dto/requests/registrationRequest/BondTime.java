a02194345637e9092860a61f74cb7c628d49cb74
anton stolyarov <a.stolyarov@g.nsu.ru>
2025-11-04 20:37:43 +0700

    added pattern for bond time fields

--- src/main/java/org/nsu/authorization/core/dto/requests/registrationRequest/BondTime.java
+++ src/main/java/org/nsu/authorization/core/dto/requests/registrationRequest/BondTime.java
@@ -1,10 +1,10 @@
 package org.nsu.authorization.core.dto.requests.registrationRequest;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
-import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalTime;
 
@@ -13,10 +13,12 @@ import java.time.LocalTime;
 @Schema(description = "Временной интервал для связи")
 public class BondTime {
     @NotNull(message = "Bond time start should not be empty")
+    @JsonFormat(pattern = "HH:mm")
     @Schema(description = "Начало временного интервала (HH:MM)", example = "10:00")
     private LocalTime bondTimeStart;
 
     @NotNull(message = "Bond time end should not be empty")
+    @JsonFormat(pattern = "HH:mm")
     @Schema(description = "Конец временного интервала (HH:MM)", example = "12:00")
     private LocalTime bondTimeEnd;
 }
