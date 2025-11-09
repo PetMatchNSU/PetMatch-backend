65d155022991cbe291f5ecb252f8cb7c13474051
d.shabanov1 <d.shabanov1@g.nsu.ru>
2025-11-09 18:02:17 +0700

    fix

--- src/main/java/org/nsu/users/repository/RegionRepository.java
+++
@@ -1,9 +0,0 @@
-package org.nsu.users.repository;
-
-import org.nsu.users.entity.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegionRepository extends JpaRepository<Region, Long> {
-}
--- src/main/java/org/nsu/users/repository/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package org.nsu.users.repository;
-
-import org.nsu.users.entity.Status;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StatusRepository extends JpaRepository<Status, Long> {
-}
