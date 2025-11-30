10378fae79d34a640c546c908db4a792a93a7e7d
anton <a.stolyarov@g.nsu.ru>
2025-11-30 13:38:50 +0700

    fixing rmeoved unused import that caused a compilation error

--- src/main/java/org/nsu/users/controllers/UserProfileController.java
+++ src/main/java/org/nsu/users/controllers/UserProfileController.java
@@ -14,9 +14,7 @@ import org.nsu.users.services.UserProfileService;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.nsu.users.dto.responses.UserAnimalListResponse;
-import org.nsu.users.dto.requests.UserAnimalListRequest;
 import org.nsu.users.services.UserAnimalListService;
 
 @RestController
