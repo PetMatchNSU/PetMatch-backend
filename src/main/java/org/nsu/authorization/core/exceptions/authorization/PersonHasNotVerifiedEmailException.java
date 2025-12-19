ffcf78bd12d5de629921d08c7ae304bd5e9646ab
anton stolyarov <a.stolyarov@g.nsu.ru>
2025-12-19 12:56:14 +0700

    removed unused exception and exception handler

--- src/main/java/org/nsu/authorization/core/exceptions/authorization/PersonHasNotVerifiedEmailException.java
+++
@@ -1,7 +0,0 @@
-package org.nsu.authorization.core.exceptions.authorization;
-
-public class PersonHasNotVerifiedEmailException extends RuntimeException {
-    public PersonHasNotVerifiedEmailException(String message) {
-        super(message);
-    }
-}
