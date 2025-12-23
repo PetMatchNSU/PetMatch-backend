b65619317919857519c170b9fcc780592fd784a4
Anton Stolyarov <146411498+helttek@users.noreply.github.com>
2025-12-23 23:12:05 +0700

    [PetMatch-16826]: /user/animals/list возвращяет пустой reviewComment
    [PetMatch-16826]: /user/animals/list возвращяет пустой reviewComment

--- src/main/java/org/nsu/admin/repositories/StatusCommentRepository.java
+++ src/main/java/org/nsu/admin/repositories/StatusCommentRepository.java
@@ -1,16 +1,26 @@
 package org.nsu.admin.repositories;
 
 import org.nsu.admin.entity.StatusComment;
+import org.nsu.animal.entity.AnimalCard;
 import org.nsu.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface StatusCommentRepository extends JpaRepository<StatusComment, Long> {
-    
+
     Optional<StatusComment> findTopByUserOrderByDateDesc(User user);
+
+    Optional<StatusComment> findTopByAnimalCardOrderByDateDesc(AnimalCard animalCard);
+
+    @Query("SELECT sc FROM StatusComment sc " +
+            "WHERE sc.animalCard.id IN :cardIds " +
+            "ORDER BY sc.animalCard.id, sc.date DESC")
+    List<StatusComment> findLatestByAnimalCardIds(@Param("cardIds") List<Long> cardIds);
+
 }
--- src/main/java/org/nsu/admin/services/StatusCommentService.java
+++ src/main/java/org/nsu/admin/services/StatusCommentService.java
@@ -3,10 +3,14 @@ package org.nsu.admin.services;
 import lombok.RequiredArgsConstructor;
 import org.nsu.admin.entity.StatusComment;
 import org.nsu.admin.repositories.StatusCommentRepository;
+import org.nsu.animal.entity.AnimalCard;
 import org.nsu.users.entity.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,4 +21,19 @@ public class StatusCommentService {
     public Optional<StatusComment> getLatestCommentByUser(User user) {
         return statusCommentRepository.findTopByUserOrderByDateDesc(user);
     }
+
+    public Optional<StatusComment> getLatestCommentByAnimalCard(AnimalCard animalCard) {
+        return statusCommentRepository.findTopByAnimalCardOrderByDateDesc(animalCard);
+    }
+
+    public Map<Long, String> getLatestCommentsByCardIds(List<Long> cardIds) {
+        List<StatusComment> comments = statusCommentRepository.findLatestByAnimalCardIds(cardIds);
+
+        return comments.stream()
+                .collect(Collectors.toMap(
+                        sc -> sc.getAnimalCard().getId(),
+                        StatusComment::getComment,
+                        (first, second) -> first));
+    }
+
 }
